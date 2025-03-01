ad465ba51930ffb49542035efe3a8a3fead2af3c
familia <familia@DESKTOP-9GRQGA3>
2025-03-01 09:02:50 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace aplicacion_broma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnsi = new System.Windows.Forms.Button();
+            this.btnno = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnno);
+            this.panel1.Controls.Add(this.btnsi);
+            this.panel1.Controls.Add(this.lbl1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl1.Font = new System.Drawing.Font("Arial", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl1.Location = new System.Drawing.Point(90, 121);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(615, 55);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "¿Deseas formatear tu pc?";
+            // 
+            // btnsi
+            // 
+            this.btnsi.Font = new System.Drawing.Font("Arial", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsi.Location = new System.Drawing.Point(192, 245);
+            this.btnsi.Name = "btnsi";
+            this.btnsi.Size = new System.Drawing.Size(133, 50);
+            this.btnsi.TabIndex = 2;
+            this.btnsi.Text = "aceptar.";
+            this.btnsi.UseVisualStyleBackColor = true;
+            // 
+            // btnno
+            // 
+            this.btnno.Font = new System.Drawing.Font("Arial", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnno.Location = new System.Drawing.Point(449, 245);
+            this.btnno.Name = "btnno";
+            this.btnno.Size = new System.Drawing.Size(133, 50);
+            this.btnno.TabIndex = 3;
+            this.btnno.Text = "rechazar.";
+            this.btnno.UseVisualStyleBackColor = true;
+            this.btnno.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnno_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnno;
+        private System.Windows.Forms.Button btnsi;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aplicacion_broma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+        public void MoverBoton()
+        {
+            Random r  = new Random();
+            int x = r.Next(0,this.Width - btnno.Width);
+            int y = r.Next(0,this.Height - btnno.Height);
+            btnno.Location = new Point(x, y); 
+
+        }
+
+        private void btnno_MouseMove(object sender, MouseEventArgs e)
+        {
+            MoverBoton();
+            if(btnno.Location == btnsi.Location || btnno.Location == lbl1.Location )
+            {
+                MoverBoton();
+            }
+        }
+    }
+}
